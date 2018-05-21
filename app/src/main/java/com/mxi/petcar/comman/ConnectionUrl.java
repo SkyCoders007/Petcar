3ccae8178cdcbd818585093ab5904ef487d0fd69
SkyProjects007 <soniakash94@gmail.com>
2018-05-21 11:00:22 +0530

    Initial commit

---
+++ app/src/main/java/com/mxi/petcar/comman/ConnectionUrl.java
@@ -0,0 +1,14 @@
+package com.mxi.petcar.comman;
+
+
+public class ConnectionUrl {
+
+    public static String baseUrl="http://mbdbtechnology.com/projects/petcare/wp-json/mxirestapi/";
+
+    public static String signup = baseUrl+"register";
+    public static String login = baseUrl+"login";
+    public static String logout = baseUrl+"logout";
+    public static String forgot = baseUrl+"forgotpassword";
+    public static String get_profile = baseUrl+"userdetail";
+    public static String edit_profile = baseUrl+"editprofile";
+}
